1811f9899f357ce0bc6293796b34028f0c57d1da
Reviz13 <hashfiputrazah@gmail.com>
2021-09-29 23:16:51 +0700

    bismilah

---
+++ component/footer.blade.php
@@ -0,0 +1,15 @@
+<div class="footer_hashfi">
+<div class="form-group row text-center">
+    <div class="col-sm-4">
+<h3> Hashfi</h3>
+    </div>
+    <div class="col-sm-4">
+        <h3> Hashfi</h3>
+    </div>
+    <div class="col-sm-4">
+        <h3> Hashfi</h3>
+    </div>
+
+</div>
+</div>
+
